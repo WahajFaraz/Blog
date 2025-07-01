31f209476ef5c3a78406f13d57a1198c8772e02b
WahajFaraz <wahajfaraz93@gmail.com>
2025-07-01 18:15:36 +0500

    Delete client directory

--- client/src/utils/PublicRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const PublicRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
-  return !token ? children : <Navigate to="/" replace />;
-};
-
-export default PublicRoute;
